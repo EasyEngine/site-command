4257cd2c06a129af25d407bb0af01fcc20b1d276
Riddhesh Sanghvi <riddhesh.sanghvi@rtcamp.com>
2018-07-03 17:02:06 +0530

    complete getacmeclient function

--- src/Site_Letsencrypt.php
+++ src/Site_Letsencrypt.php
@@ -1,42 +1,71 @@
 <?php
 
+use AcmePhp\Cli\Repository\Repository;
 use AcmePhp\Cli\Serializer\PemEncoder;
 use AcmePhp\Cli\Serializer\PemNormalizer;
+use AcmePhp\Core\AcmeClient;
+use AcmePhp\Core\Http\SecureHttpClient;
+use AcmePhp\Core\Http\Base64SafeEncoder;
+use AcmePhp\Core\Http\ServerErrorHandler;
+use AcmePhp\Ssl\Parser\KeyParser;
+use AcmePhp\Ssl\Generator\KeyPairGenerator;
+use AcmePhp\Ssl\Signer\CertificateRequestSigner;
+use AcmePhp\Ssl\Signer\DataSigner;
+
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use League\Flysystem\Filesystem;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Adapter\NullAdapter;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+
 
 class Site_Letsencrypt {
 
-	public function getAcmeClient(){
-		// Get repository class working.
+	public function getAcmeClient() {
+
 		$serializer = new Serializer(
-			[new PemNormalizer(), new GetSetMethodNormalizer()],
-			[new PemEncoder(), new JsonEncoder()]
+			[ new PemNormalizer(), new GetSetMethodNormalizer() ],
+			[ new PemEncoder(), new JsonEncoder() ]
 		);
-		// TODO: change directory for master fs
-		$master = new Filesystem( new Local( EE_CONF_ROOT . '/master' ) );
-		$backup = new Filesystem( new NullAdapter() );
-		$repository = new \AcmePhp\Cli\Repository\Repository( $serializer, $master, $backup, false );
 
-		if (!$repository->hasAccountKeyPair()) {
-            EE::log('No account key pair was found, generating one...');
-            EE::log('Generating a key pair');
+		$master     = new Filesystem( new Local( EE_CONF_ROOT . '/master' ) );
+		$backup     = new Filesystem( new NullAdapter() );
+		$repository = new Repository( $serializer, $master, $backup, false );
 
-			/** @var KeyPair $accountKeyPair */
-			$keygen = new AcmePhp\Ssl\Generator\KeyPairGenerator();
-            $accountKeyPair = $keygen->generateKeyPair();
+		if ( ! $repository->hasAccountKeyPair() ) {
+			EE::debug( 'No account key pair was found, generating one...' );
+			EE::debug( 'Generating a key pair' );
 
-            EE::log('Key pair generated, storing');
-            $repository->storeAccountKeyPair($accountKeyPair);
+			$keygen         = new KeyPairGenerator();
+			$accountKeyPair = $keygen->generateKeyPair();
+			EE::debug( 'Key pair generated, storing' );
+			$repository->storeAccountKeyPair( $accountKeyPair );
+		} else {
+			EE::debug( 'Loading account keypair' );
+			$accountKeyPair = $repository->loadAccountKeyPair();
 		}
-		EE::log('Loading acc keypair');
 
-		// $core = new \AcmePhp\Core\AcmeClient();
-		// $ssl = new \AcmePhp\Ssl\Certificate();
+		$httpClient         = new Client();
+		$serverErrorHandler = new ServerErrorHandler();
+		$base64SafeEncoder  = new Base64SafeEncoder();
+		$keyParser          = new KeyParser();
+		$dataSigner         = new DataSigner();
+
+		$secureHttpClient = new SecureHttpClient(
+			$accountKeyPair,
+			$httpClient,
+			$base64SafeEncoder,
+			$keyParser,
+			$dataSigner,
+			$serverErrorHandler
+		);
+		$csrSigner        = new CertificateRequestSigner();
+
+		return new AcmeClient( $secureHttpClient, 'https://acme-v02.api.letsencrypt.org/directory', $csrSigner );
+
 	}
 
 }
