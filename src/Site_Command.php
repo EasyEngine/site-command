4a426efff2ccdd0c98aaee02c196b77def41c532
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-07-25 17:19:47 +0530

    Remove wordpress installation
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Site_Command.php
+++ src/Site_Command.php
@@ -948,33 +948,6 @@ server {
 
 
 	/**
-	 * Install wordpress with given credentials.
-	 */
-	private function install_wp() {
-		EE::log( "\nInstalling WordPress site." );
-		chdir( $this->site_root );
-
-		$wp_install_command = 'install';
-		$maybe_multisite_type    = '';
-
-		if ( 'wpsubdom' === $this->site_type || 'wpsubdir' === $this->site_type ) {
-			$wp_install_command = 'multisite-install';
-			$maybe_multisite_type  = $this->site_type === 'wpsubdom' ? '--subdomains' : '';
-		}
-
-		$install_command = "docker-compose exec --user='www-data' php wp core $wp_install_command --url='$this->site_name' --title='$this->site_title' --admin_user='$this->site_user'" . ( $this->site_pass ? " --admin_password='$this->site_pass'" : '' ) . " --admin_email='$this->site_email' $maybe_multisite_type";
-
-		$core_install = \EE\Utils\default_launch( $install_command );
-
-		if ( ! $core_install ) {
-			EE::warning( 'WordPress install failed. Please check logs.' );
-		}
-
-		$prefix = ( $this->le ) ? 'https://' : 'http://';
-		EE::success( $prefix . $this->site_name . " has been created successfully!" );
-	}
-
-	/**
 	 * Function to save the site configuration entry into database.
 	 */
 	private function create_site_db_entry() {
