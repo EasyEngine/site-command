e9f1b66000517c3099ccfe39912eaf1d3b774832
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-07-31 23:51:55 +0530

    Refactoring and update for unification of code

--- src/Site_Command.php
+++ src/Site_Command.php
@@ -29,6 +29,7 @@ class Site_Command extends EE_Site_Command {
 	private $fs;
 
 	public function __construct() {
+
 		$this->level   = 0;
 		$this->command = 'site';
 		pcntl_signal( SIGTERM, [ $this, "rollback" ] );
@@ -62,12 +63,12 @@ class Site_Command extends EE_Site_Command {
 	 */
 	public function create( $args, $assoc_args ) {
 
-		\EE\Utils\delem_log( 'site create start' );
+		EE\Utils\delem_log( 'site create start' );
 		EE::warning( 'This is a beta version. Please don\'t use it in production.' );
 		$this->logger->debug( 'args:', $args );
-		$this->logger->debug( 'assoc_args:', empty( $assoc_args ) ? array( 'NULL' ) : $assoc_args );
-		$this->site_name = strtolower( \EE\Utils\remove_trailing_slash( $args[0] ) );
-		$this->site_type = \EE\Utils\get_flag_value( $assoc_args, 'type', 'html' );
+		$this->logger->debug( 'assoc_args:', empty( $assoc_args ) ? [ 'NULL' ] : $assoc_args );
+		$this->site_name = strtolower( EE\Utils\remove_trailing_slash( $args[0] ) );
+		$this->site_type = EE\Utils\get_flag_value( $assoc_args, 'type', 'html' );
 		if ( 'html' !== $this->site_type ) {
 			EE::error( "Invalid site-type: $this->site_type" );
 		}
@@ -76,15 +77,15 @@ class Site_Command extends EE_Site_Command {
 			EE::error( "Site $this->site_name already exists. If you want to re-create it please delete the older one using:\n`ee site delete $this->site_name`" );
 		}
 
-		$this->le       = \EE\Utils\get_flag_value( $assoc_args, 'letsencrypt' );
-		$this->skip_chk = \EE\Utils\get_flag_value( $assoc_args, 'skip-status-check' );
+		$this->le       = EE\Utils\get_flag_value( $assoc_args, 'letsencrypt' );
+		$this->skip_chk = EE\Utils\get_flag_value( $assoc_args, 'skip-status-check' );
 
-		\EE\SiteUtils\init_checks();
+		EE\SiteUtils\init_checks();
 
 		EE::log( 'Configuring project.' );
 
 		$this->create_site();
-		\EE\Utils\delem_log( 'site create end' );
+		EE\Utils\delem_log( 'site create end' );
 	}
 
 	/**
@@ -94,24 +95,25 @@ class Site_Command extends EE_Site_Command {
 	 * : Name of the website whose info is required.
 	 */
 	public function info( $args, $assoc_args ) {
-		\EE\Utils\delem_log( 'site info start' );
+
+		EE\Utils\delem_log( 'site info start' );
 		if ( ! isset( $this->site_name ) ) {
-			$args = \EE\SiteUtils\auto_site_name( $args, $this->command, __FUNCTION__ );
+			$args = EE\SiteUtils\auto_site_name( $args, $this->command, __FUNCTION__ );
 			$this->populate_site_info( $args );
 		}
 		$ssl = $this->le ? 'Enabled' : 'Not Enabled';
 		EE::log( "Details for site $this->site_name:" );
 		$prefix = ( $this->le ) ? 'https://' : 'http://';
-		$info   = array(
-			array( 'Site', $prefix . $this->site_name ),
-			array( 'Access mailhog', $prefix . $this->site_name . '/ee-admin/mailhog/' ),
-			array( 'Site Root', $this->site_root ),
-			array( 'SSL', $ssl ),
-		);
+		$info   = [
+			[ 'Site', $prefix . $this->site_name ],
+			[ 'Access mailhog', $prefix . $this->site_name . '/ee-admin/mailhog/' ],
+			[ 'Site Root', $this->site_root ],
+			[ 'SSL', $ssl ],
+		];
 
-		\EE\Utils\format_table( $info );
+		EE\Utils\format_table( $info );
 
-		\EE\Utils\delem_log( 'site info end' );
+		EE\Utils\delem_log( 'site info end' );
 	}
 
 
@@ -130,19 +132,19 @@ class Site_Command extends EE_Site_Command {
 		EE::log( "Creating site $this->site_name." );
 		EE::log( 'Copying configuration files.' );
 
-		$filter                 = array();
+		$filter                 = [];
 		$filter[]               = $this->site_type;
 		$filter[]               = $this->le;
 		$site_docker            = new Site_Docker();
 		$docker_compose_content = $site_docker->generate_docker_compose_yml( $filter );
-		$default_conf_content   = $default_conf_content = \EE\Utils\mustache_render( SITE_TEMPLATE_ROOT . '/config/nginx/default.conf.mustache', [ 'server_name' => $this->site_name ] );
+		$default_conf_content   = $default_conf_content = EE\Utils\mustache_render( SITE_TEMPLATE_ROOT . '/config/nginx/default.conf.mustache', [ 'server_name' => $this->site_name ] );
 
 		$env_data    = [
 			'virtual_host' => $this->site_name,
 			'user_id'      => $process_user['uid'],
 			'group_id'     => $process_user['gid'],
 		];
-		$env_content = \EE\Utils\mustache_render( SITE_TEMPLATE_ROOT . '/config/.env.mustache', $env_data );
+		$env_content = EE\Utils\mustache_render( SITE_TEMPLATE_ROOT . '/config/.env.mustache', $env_data );
 
 		try {
 			$this->fs->dumpFile( $site_docker_yml, $docker_compose_content );
@@ -155,15 +157,14 @@ class Site_Command extends EE_Site_Command {
 				'version'       => 'v' . EE_VERSION,
 				'site_src_root' => $this->site_root . '/app/src',
 			];
-			$index_html = \EE\Utils\mustache_render( SITE_TEMPLATE_ROOT . '/index.html.mustache', $index_data );
+			$index_html = EE\Utils\mustache_render( SITE_TEMPLATE_ROOT . '/index.html.mustache', $index_data );
 			$this->fs->mkdir( $site_src_dir );
 			$this->fs->dumpFile( $site_src_dir . '/index.html', $index_html );
 
 			EE\Siteutils\add_site_redirects( $this->site_name, $this->le );
 
 			EE::success( 'Configuration files copied.' );
-		}
-		catch ( Exception $e ) {
+		} catch ( Exception $e ) {
 			$this->catch_clean( $e );
 		}
 	}
@@ -172,6 +173,7 @@ class Site_Command extends EE_Site_Command {
 	 * Function to create the site.
 	 */
 	private function create_site() {
+
 		$this->site_root = WEBROOT . $this->site_name;
 		$this->level     = 1;
 		try {
@@ -188,8 +190,7 @@ class Site_Command extends EE_Site_Command {
 				$this->level = 4;
 				EE\Siteutils\site_status_check( $this->site_name );
 			}
-		}
-		catch ( Exception $e ) {
+		} catch ( Exception $e ) {
 			$this->catch_clean( $e );
 		}
 
@@ -204,15 +205,16 @@ class Site_Command extends EE_Site_Command {
 	 * Function to save the site configuration entry into database.
 	 */
 	private function create_site_db_entry() {
+
 		$ssl  = $this->le ? 1 : 0;
-		$data = array(
+		$data = [
 			'sitename'     => $this->site_name,
 			'site_type'    => $this->site_type,
 			'site_path'    => $this->site_root,
 			'site_command' => $this->command,
 			'is_ssl'       => $ssl,
 			'created_on'   => date( 'Y-m-d H:i:s', time() ),
-		);
+		];
 
 		try {
 			if ( $this->db::insert( $data ) ) {
@@ -220,8 +222,7 @@ class Site_Command extends EE_Site_Command {
 			} else {
 				throw new Exception( 'Error creating site entry in database.' );
 			}
-		}
-		catch ( Exception $e ) {
+		} catch ( Exception $e ) {
 			$this->catch_clean( $e );
 		}
 	}
@@ -231,15 +232,15 @@ class Site_Command extends EE_Site_Command {
 	 */
 	private function populate_site_info( $args ) {
 
-		$this->site_name = \EE\Utils\remove_trailing_slash( $args[0] );
+		$this->site_name = EE\Utils\remove_trailing_slash( $args[0] );
 
 		if ( $this->db::site_in_db( $this->site_name ) ) {
 
-			$db_select = $this->db::select( [], array( 'sitename' => $this->site_name ) );
+			$db_select = $this->db::select( [], [ 'sitename' => $this->site_name ], 'sites', 1 );
 
-			$this->site_type = $db_select[0]['site_type'];
-			$this->site_root = $db_select[0]['site_path'];
-			$this->le        = $db_select[0]['is_ssl'];
+			$this->site_type = $db_select['site_type'];
+			$this->site_root = $db_select['site_path'];
+			$this->le        = $db_select['is_ssl'];
 
 		} else {
 			EE::error( "Site $this->site_name does not exist." );
@@ -311,18 +312,18 @@ class Site_Command extends EE_Site_Command {
 		return parent::le( $args, $assoc_args, $wildcard );
 	}
 
-
 	/**
 	 * Catch and clean exceptions.
 	 *
 	 * @param Exception $e
 	 */
 	private function catch_clean( $e ) {
-		\EE\Utils\delem_log( 'site cleanup start' );
+
+		EE\Utils\delem_log( 'site cleanup start' );
 		EE::warning( $e->getMessage() );
 		EE::warning( 'Initiating clean-up.' );
 		$this->delete_site( $this->level, $this->site_name, $this->site_root );
-		\EE\Utils\delem_log( 'site cleanup end' );
+		EE\Utils\delem_log( 'site cleanup end' );
 		exit;
 	}
 
@@ -330,6 +331,7 @@ class Site_Command extends EE_Site_Command {
 	 * Roll back on interrupt.
 	 */
 	private function rollback() {
+
 		EE::warning( 'Exiting gracefully after rolling back. This may take some time.' );
 		if ( $this->level > 0 ) {
 			$this->delete_site( $this->level, $this->site_name, $this->site_root );
@@ -342,16 +344,15 @@ class Site_Command extends EE_Site_Command {
 	 * Shutdown function to catch and rollback from fatal errors.
 	 */
 	private function shutDownFunction() {
+
 		$error = error_get_last();
-		if ( isset( $error ) ) {
-			if ( $error['type'] === E_ERROR ) {
-				EE::warning( 'An Error occurred. Initiating clean-up.' );
-				$this->logger->error( 'Type: ' . $error['type'] );
-				$this->logger->error( 'Message: ' . $error['message'] );
-				$this->logger->error( 'File: ' . $error['file'] );
-				$this->logger->error( 'Line: ' . $error['line'] );
-				$this->rollback();
-			}
+		if ( isset( $error ) && $error['type'] === E_ERROR ) {
+			EE::warning( 'An Error occurred. Initiating clean-up.' );
+			$this->logger->error( 'Type: ' . $error['type'] );
+			$this->logger->error( 'Message: ' . $error['message'] );
+			$this->logger->error( 'File: ' . $error['file'] );
+			$this->logger->error( 'Line: ' . $error['line'] );
+			$this->rollback();
 		}
 	}
 }
--- src/Site_Docker.php
+++ src/Site_Docker.php
@@ -12,70 +12,66 @@ class Site_Docker {
 	 * @return String docker-compose.yml content string.
 	 */
 	public function generate_docker_compose_yml( array $filters = [] ) {
-		$base = array();
+		$base = [];
 
-		$restart_default = array( 'name' => 'always' );
-		$network_default = array( 'name' => 'site-network' );
+		$restart_default = [ 'name' => 'always' ];
+		$network_default = [ 'name' => 'site-network' ];
 
 		// nginx configuration.
-		$nginx['service_name'] = array( 'name' => 'nginx' );
-		$nginx['image']        = array( 'name' => 'easyengine/nginx:v' . EE_VERSION );
+		$nginx['service_name'] = [ 'name' => 'nginx' ];
+		$nginx['image']        = [ 'name' => 'easyengine/nginx:v' . EE_VERSION ];
 		$nginx['restart']      = $restart_default;
 
 		$v_host = 'VIRTUAL_HOST';
 
-		$nginx['environment'] = array(
-			'env' => array(
-				array( 'name' => $v_host ),
-				array( 'name' => 'VIRTUAL_PATH=/' ),
-				array( 'name' => 'HSTS=off' ),
-			),
-		);
-		$nginx['volumes']     = array(
-			'vol' => array(
-				array( 'name' => './app/src:/var/www/htdocs' ),
-				array( 'name' => './config/nginx/default.conf:/etc/nginx/conf.d/default.conf' ),
-				array( 'name' => './logs/nginx:/var/log/nginx' ),
-				array( 'name' => './config/nginx/common:/usr/local/openresty/nginx/conf/common' ),
-			),
-		);
-		$nginx['labels']      = array(
-			array(
-				'label' => array(
-					'name' => 'io.easyengine.site=${VIRTUAL_HOST}',
-				),
-			),
-		);
+		$nginx['environment'] = [
+			'env' => [
+				[ 'name' => $v_host ],
+				[ 'name' => 'VIRTUAL_PATH=/' ],
+				[ 'name' => 'HSTS=off' ],
+			],
+		];
+		$nginx['volumes']     = [
+			'vol' => [
+				[ 'name' => './app/src:/var/www/htdocs' ],
+				[ 'name' => './config/nginx/default.conf:/etc/nginx/conf.d/default.conf' ],
+				[ 'name' => './logs/nginx:/var/log/nginx' ],
+				[ 'name' => './config/nginx/common:/usr/local/openresty/nginx/conf/common' ],
+			],
+		];
+		$nginx['labels']      = [
+			'label' => [
+				'name' => 'io.easyengine.site=${VIRTUAL_HOST}',
+			],
+		];
 		$nginx['networks']    = $network_default;
 
 		// mailhog configuration.
-		$mailhog['service_name'] = array( 'name' => 'mailhog' );
-		$mailhog['image']        = array( 'name' => 'easyengine/mailhog:v' . EE_VERSION );
+		$mailhog['service_name'] = [ 'name' => 'mailhog' ];
+		$mailhog['image']        = [ 'name' => 'easyengine/mailhog:v' . EE_VERSION ];
 		$mailhog['restart']      = $restart_default;
-		$mailhog['command']      = array( 'name' => '["-invite-jim=false"]' );
-		$mailhog['environment']  = array(
-			'env' => array(
-				array( 'name' => $v_host ),
-				array( 'name' => 'VIRTUAL_PATH=/ee-admin/mailhog/' ),
-				array( 'name' => 'VIRTUAL_PORT=8025' ),
-			),
-		);
-		$mailhog['labels']       = array(
-			array(
-				'label' => array(
-					'name' => 'io.easyengine.site=${VIRTUAL_HOST}',
-				),
-			),
-		);
+		$mailhog['command']      = [ 'name' => '["-invite-jim=false"]' ];
+		$mailhog['environment']  = [
+			'env' => [
+				[ 'name' => $v_host ],
+				[ 'name' => 'VIRTUAL_PATH=/ee-admin/mailhog/' ],
+				[ 'name' => 'VIRTUAL_PORT=8025' ],
+			],
+		];
+		$mailhog['labels']       = [
+			'label' => [
+				'name' => 'io.easyengine.site=${VIRTUAL_HOST}',
+			],
+		];
 		$mailhog['networks']     = $network_default;
 
 		$base[] = $nginx;
 		$base[] = $mailhog;
 
-		$binding = array(
+		$binding = [
 			'services' => $base,
 			'network'  => true,
-		);
+		];
 
 		$docker_compose_yml = mustache_render( SITE_TEMPLATE_ROOT . '/docker-compose.mustache', $binding );
 
