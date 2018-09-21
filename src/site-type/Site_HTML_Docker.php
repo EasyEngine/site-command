dcf20946617cf8cc784f05c8e3786a4e78e4cfc6
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-09-21 16:52:19 +0530

    Remove common conf directory mount
    Signed-off-by: Kirtan Gajjar <kirtangajjar95@gmail.com>

--- src/site-type/Site_HTML_Docker.php
+++ src/site-type/Site_HTML_Docker.php
@@ -44,7 +44,6 @@ class Site_HTML_Docker {
 				[ 'name' => './config/nginx/main.conf:/etc/nginx/conf.d/default.conf' ],
 				[ 'name' => './config/nginx/custom:/etc/nginx/custom' ],
 				[ 'name' => './logs/nginx:/var/log/nginx' ],
-				[ 'name' => './config/nginx/common:/usr/local/openresty/nginx/conf/common' ],
 			],
 		];
 		$nginx['labels']      = [
