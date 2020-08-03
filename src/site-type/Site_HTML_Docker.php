f4467db669d473e9997da9b179f3fb0f94ab70ec
dhsathiya <devarshisathiya5@gmail.com>
2020-08-03 16:57:43 +0530

    Add sysctl parameters in site type HTML
    Signed-off-by: dhsathiya <devarshisathiya5@gmail.com>

--- src/site-type/Site_HTML_Docker.php
+++ src/site-type/Site_HTML_Docker.php
@@ -4,6 +4,7 @@ namespace EE\Site\Type;
 
 use function EE\Utils\mustache_render;
 use function EE\Site\Utils\get_ssl_policy;
+use function EE\Site\Utils\sysctl_parameters;
 
 class Site_HTML_Docker {
 
@@ -51,6 +52,7 @@ class Site_HTML_Docker {
 				'name' => 'io.easyengine.site=${VIRTUAL_HOST}',
 			],
 		];
+		$nginx['sysctls']  = sysctl_parameters();
 		$nginx['networks'] = [
 			'net' => [
 				[ 'name' => 'global-frontend-network' ],
