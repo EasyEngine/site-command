cbf82df4d7d98722051a57aa4653917e600d41c1
Your Name <you@example.com>
2021-02-19 12:47:23 +0000

    Updated code for php 8.0 compatibility

--- src/helper/Site_Letsencrypt.php
+++ src/helper/Site_Letsencrypt.php
@@ -306,7 +306,7 @@ class Site_Letsencrypt {
 
 			try {
 				$this->client->revokeCertificate( $certificate, $revocationReason );
-				$domain = gettype( $domain ) === 'string' ? $domain : get_class( $domain ) === 'AcmePhp\Ssl\Certificate' ? array_search( $domain, $domains ) : '';
+				$domain = ( gettype( $domain ) === 'string' ? $domain : get_class( $domain ) === 'AcmePhp\Ssl\Certificate' ) ? array_search( $domain, $domains ) : '';
 				\EE::debug( 'Certificate for ' . $domain . ' revoked successfully' );
 			} catch ( CertificateRevocationException $e ) {
 				\EE::debug( $e->getMessage() );
