dc16ea7f328f5688f85852dac6743a726e135677
ILYES ABDELRAZAK BELADEL <ia.beladel@gmail.com>
2021-05-02 23:45:58 +0000

    Make ee compatible with PHP 8.0

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -380,7 +380,7 @@ class FeatureContext implements Context
 	public function thereShouldBeContainersWithLabel($expected_running_containers, PyStringNode $pyStringNode)
 	{
 		$labels = $pyStringNode->getStrings();
-		$label_string = implode($labels, ' -f label=');
+		$label_string = implode(' -f label=', $labels);
 
 		$result = EE::launch( "docker ps -qf label=$label_string | wc -l", false, true );
 		$running_containers = (int) trim($result->stdout);
