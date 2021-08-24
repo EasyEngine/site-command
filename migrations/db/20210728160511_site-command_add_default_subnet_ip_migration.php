fbfb1c4ec8d848798607571a9adfa5e9f55070c8
Kirtan Gajjar <kirtangajjar95@gmail.com>
2021-08-24 15:14:42 +0530

    Fix site limit migration

--- migrations/db/20210728160511_site-command_add_default_subnet_ip_migration.php
+++ migrations/db/20210728160511_site-command_add_default_subnet_ip_migration.php
@@ -4,6 +4,8 @@ namespace EE\Migration;
 use EE;
 use EE\Model\Site;
 use PDOException;
+use function EE\Site\Utils\auto_site_name;
+use function EE\Site\Utils\get_site_info;
 
 class AddDefaultSubnetIpMigration extends Base {
 
@@ -45,6 +47,12 @@ class AddDefaultSubnetIpMigration extends Base {
 			$site_type = $site->site_type === 'html' ? new EE\Site\Type\HTML() :
 						( $site->site_type === 'php' ? new EE\Site\Type\PHP() :
 						( $site->site_type === 'wp' ? new EE\Site\Type\WordPress()  : EE::error('Unknown site type') ) );
+
+			$site_type->site_data = get_site_info( [ $site->site_url ], false, true, false );
+			$site_data = (array) $site_type->site_data;
+			$site_data = reset($site_data );
+			$site_type->site_data = $site_data;
+			$site_type->dump_docker_compose_yml();
 
 			if ( $site->site_enabled ) {
 				$site_type->refresh( [ $site->site_url ], [] );
