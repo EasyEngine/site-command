ff5ac9b1405f70fe7b2efa24ad7828dce4a11d5b
Kirtan Gajjar <kirtangajjar95@gmail.com>
2021-09-02 12:33:49 +0530

    Add migration for iproute2

---
+++ migrations/container/20210902112747_site-command_install_iproute2.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EE\Migration;
+
+use EE;
+use EE\Migration\Base;
+use EE\RevertableStepProcessor;
+use EE\Model\Site;
+
+class InstallIproute2 extends Base {
+
+	private $sites;
+	/** @var RevertableStepProcessor $rsp Keeps track of migration state. Reverts on error */
+	private static $rsp;
+
+	/**
+	 * Execute php config updates.
+	 *
+	 * @throws EE\ExitException
+	 */
+	public function up() {
+
+		if ( $this->skip_this_migration ) {
+			EE::debug( 'Skipping add-ext-config-8.0 update migration as it is not needed.' );
+
+			return;
+		}
+		EE::exec( 'sudo apt install iproute2' );
+	}
+
+	/**
+	 * Bring back the existing old php config.
+	 *
+	 * @throws EE\ExitException
+	 */
+	public function down() {
+
+	}
+}
