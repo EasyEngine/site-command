c3ec4914ee7490d72e299cbde3a36a15fef8178a
Kirtan Gajjar <kirtangajjar95@gmail.com>
2021-10-20 17:31:22 +0530

    Remove mac message for daemon.json

--- migrations/db/20210728160510_site-command_add_daemon_json.php
+++ migrations/db/20210728160510_site-command_add_daemon_json.php
@@ -20,27 +20,7 @@ class AddDaemonJson extends Base {
 	 */
 	public function up() {
 
-		if ( IS_DARWIN ) {
-			// This message also needs to be show in installer too.
-			EE::log( PHP_EOL . PHP_EOL . 'Hey there, we have recently introduced a feature
-which would allow you to create more than 27 sites.
-
-To enable it, you need to perform the following steps manually:
-1. Please open Docker Desktop and in taskbar, go to Preferences > Daemon > Advanced.
-2. If the file is empty, add the following:
-
-{
- "default-address-pools": [{"base":"10.0.0.0/8","size":24}]
-}
-
-If the file already contains JSON, just add the key "default-address-pools": [{"base":"10.0.0.0/8","size":24}]
-being careful to add a comma to the end of the line if it is not the last line before the closing bracket.
-
-3. Restart Docker' . PHP_EOL );
-
-			EE::confirm( 'Once done with above changes, please press y' );
-
-		} elseif ( php_uname('s') === 'Linux' ) {
+		if ( php_uname('s') === 'Linux' ) {
 			if ( file_exists( '/etc/docker/daemon.json' ) ) {
 				$existin_config = file_get_contents( '/etc/docker/daemon.json' );
 				$existin_config = json_decode( $existin_config, true );
